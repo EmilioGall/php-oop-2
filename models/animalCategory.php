b50f15fa90dd87e50855b4d21a939b60c428f233
Emilio Gallo <emilio.gallo17@gmail.com>
2024-06-03 16:43:08 +0200

    feat: added scaffolding & animalCategory.php

---
+++ models/animalCategory.php
@@ -0,0 +1,27 @@
+<?php
+
+class AnimalCategory
+{
+
+   private string $species;
+   private string $size;
+
+   public function __construct(string $_species, string $_size)
+   {
+
+      $this->species = $_species;
+      $this->size    = $_size;
+   }
+
+   public function getSpecies()
+   {
+
+      return  $this->species;
+   }
+
+   public function getSize()
+   {
+
+      return  $this->size;
+   }
+}
---
+++ models/product.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once __DIR__ . '/animalCategory.php';
+
+
+class Product
+{
+
+   private string $name;
+   private int    $price;
+   private AnimalCategory $animal_category;
+
+
+
+
+
+}
