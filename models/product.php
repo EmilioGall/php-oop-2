10dd603a940c12f953b44cbc068c23f5dcf8c787
Emilio Gallo <emilio.gallo17@gmail.com>
2024-06-03 16:51:00 +0200

    feat: added class - product.php

--- models/product.php
+++ models/product.php
@@ -10,8 +10,30 @@ class Product
    private int    $price;
    private AnimalCategory $animal_category;
 
+   public function __construct(string $_name, int $_price, AnimalCategory $_animal_category)
+   {
 
+      $this->name   = $_name;
+      $this->price  = $_price;
+      $this->animal_category  = $_animal_category;
+   }
 
+   public function getName()
+   {
 
+      return  $this->name;
+   }
+
+   public function getPrice()
+   {
+
+      return  $this->price;
+   }
+
+   public function getCategory()
+   {
+
+      return  $this->animal_category;
+   }
 
 }
